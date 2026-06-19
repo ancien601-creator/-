342dc0718d56607d81c45f35c60f4514b4101319
ancien601-creator <ancien601@gmail.com>
2026-06-19 10:27:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
-BOT_TOKEN = "ТВОЙ_ТЕЛЕГРАМ_ТОКЕН"  # Замени на свой токен
-DB_PATH = "giveaway.db"
+# Токен берем из переменных окружения Railway
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Путь к БД: если запущен на сервере, сохраняем на постоянный диск /data
+if os.getenv("RAILWAY_ENVIRONMENT_NAME"):
+    DB_PATH = "/data/giveaway.db"
+else:
+    DB_PATH = "giveaway.db"  # Локально при разработке
