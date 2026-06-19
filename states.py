144056f82f58d89baac2a22560643ba9d23a96b9
ancien601-creator <ancien601@gmail.com>
2026-06-19 12:44:59 +0300

    Refactor contest states and add new creation classes

--- states.py
+++ states.py
@@ -1,9 +1,22 @@
-from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.state import State, StatesGroup
 
-class CreateContest(StatesGroup):
-    channel = State()        # Ввод ID или username целевого канала
-    content = State()        # Текст и/или фото конкурса
-    finish_type = State()    # Выбор условия завершения
-    winners_count = State()  # Количество победителей
-    button_text = State()    # Текст на инлайн-кнопке под постом
-    sponsors = State()       # Добавление спонсорских каналов
+class ClassicCreation(StatesGroup):
+    channel = State()
+    content_text = State()
+    content_photo = State()
+    end_condition = State()
+    end_value = State()
+    winners_count = State()
+    button_text = State()
+    sponsors = State()
+    confirm = State()
+
+class SlotsCreation(StatesGroup):
+    channel = State()
+    content_text = State()
+    content_photo = State()
+    slots_count = State()
+    payment = State()
+    slot_price = State()
+    sponsors = State()
+    confirm = State()
