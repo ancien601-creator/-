76b28179fdb420fb6a141583b48c381a5b08f76c
ancien601-creator <ancien601@gmail.com>
2026-06-19 12:51:51 +0300

    Update DB_NAME to use DATA_DIR environment variable

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import aiosqlite
 import json
 from typing import Optional
 
-DB_NAME = "bot.db"
+DB_NAME = os.path.join(os.getenv("DATA_DIR", "/app/data"), "bot.db")
 
 async def get_db():
     return await aiosqlite.connect(DB_NAME)
