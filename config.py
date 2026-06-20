0cf7698eeebda0a51ca18ffcdd2d7eec1f8bfb73
ancien601-creator <ancien601@gmail.com>
2026-06-20 19:48:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8821231189:AAE6rlFTF4iIR7r-YtOBLVpqD0XHnlw3avk")
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
 DB_PATH = os.getenv("DB_PATH", "giveaway.db")
